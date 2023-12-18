bd30abc34d4bc027591e4bd582231d7e670c0e33
Dmitry Anufriev <dm.anufriev@gmail.com>
2023-12-18 10:55:55 +0300

    Step 4. Add arguments to application

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,12 +4,18 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
-
 import java.util.concurrent.Callable;
+import java.util.List;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 public class App implements Callable<Integer> {
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    String format;
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    String filePath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    String filePath2;
     @Override
     public Integer call() throws Exception {
         System.out.println("Hello world!");
