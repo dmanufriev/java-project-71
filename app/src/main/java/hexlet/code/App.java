a09e071ed7309f847bc2eb0cfb0070e10412cf89
Dmitry Anufriev <dm.anufriev@gmail.com>
2023-12-18 10:16:34 +0300

    Step 3. Add picocli to the project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,23 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Hello world!");
+        return 0;
+    }
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
 }
